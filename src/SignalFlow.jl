7da288fee2be19ddc354fc5efa5cf2c0ae9680d6
TANIGUCHI Kazushige <taniguchi.kazushige@gmail.com>
2026-01-15 00:54:33 +0900

    append LPF

--- src/SignalFlow.jl
+++ src/SignalFlow.jl
@@ -5,6 +5,7 @@ function input! end
 
 include("ADFMCOMMS2Src.jl")
 include("FFTView.jl")
+include("LPF.jl")
 
 function append_block!(src::SignalFlowBlock, sink::SignalFlowBlock)
     put!(src.new_sinks, sink)
